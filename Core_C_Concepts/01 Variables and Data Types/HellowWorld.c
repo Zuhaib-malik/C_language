a5dc7d3e4cbef95b23782ea123c0fa51e07edc95
Muhammad Zuhaib <mzuhaibmalik786@gmail.com>
2025-07-18 03:08:05 +0500

    Added code

---
+++ Core_C_Concepts/01 Variables and Data Types/HellowWorld.c
@@ -0,0 +1,8 @@
+// this is the hellow world in c programming
+// this main function is the entry point from where the code starts execution 
+
+#include <stdio.h>
+void main(){
+    printf("Hellow World");
+}
+
